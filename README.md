6168a7d77aee7b5bf5e7a8fb3939b9e91a5e4e09
TheRod1990 <Daniel.Beller@me.com>
2018-11-30 13:43:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dbmedia
+"# NodeJS-Boilerplate" 
---
+++ app.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var debug = require('debug')('workspace:server');
+var http = require('http');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var server  = require('http').createServer(app);
+
+//--------------------------------------------------------------------
+//PORT CONFIG
+
+var port = normalizePort(process.env.PORT || '3003');
+app.set('port', port);
+var server = http.Server(app);
+var io = require('socket.io').listen(server);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+        case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+        default:
+        throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+}
+
+
+// -------------------------------------------------------------
+//VIEW ENGINE SETUP
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// TO DO! FAVICON! 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    res.render('index', {
+        title: 'Hello Title!'
+    });
+});
+
+//--------------------------------------------------------------------------------------------------------------
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
